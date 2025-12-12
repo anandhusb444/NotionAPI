bf951303308c5d22f2df3c8b47ac772d05fde19d
anandhu sb <anandhusb444@gmail.com>
2025-12-12 23:42:03 +0530

    Added user service to add user

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,10 +1,37 @@
-﻿namespace NotionAPI.Services
+﻿using Microsoft.EntityFrameworkCore;
+using NotionAPI.Context;
+using NotionAPI.Models;
+
+namespace NotionAPI.Services
 {
     public interface IuserService
     {
-        public bool AddUser();
+        public Task<bool> AddUser(Users user);
     }
-    public class UserService
+    public class UserService : IuserService
     {
+        public NotionData _context;
+
+        public UserService(NotionData context)
+        {
+            _context = context;
+        }
+        public async Task<bool> AddUser(Users user)
+        {
+            //check the user already exist in the databse
+
+            Users currentUser = await _context.users.Where(u => u.Email == user.Email).FirstOrDefaultAsync();
+
+            if(currentUser != null)
+            {
+                return false;
+            }
+
+            await _context.users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        
     }
 }
