bc9c9c9f4d85edf92edcd10c6c1a3a47ad358270
anandhu sb <anandhusb444@gmail.com>
2025-12-21 23:30:18 +0530

    Fixed the token related bug

--- Services/TasksServices.cs
+++ Services/TasksServices.cs
@@ -1,4 +1,6 @@
-﻿using NotionAPI.DTOs.Task;
+﻿using NotionAPI.Context;
+using NotionAPI.DTOs.Task;
+using NotionAPI.Models;
 using NotionAPI.Utilites;
 
 namespace NotionAPI.Services
@@ -6,7 +8,7 @@ namespace NotionAPI.Services
     public interface ITasksServices
     {
         //Add
-        //Task<GenericRespones<Task>> AddTasks(TaskDto task);
+        Task<GenericRespones<TodoTasks>> AddTasks(int userId,TaskDto task);
         //update
         //delete
         //get
@@ -14,6 +16,45 @@ namespace NotionAPI.Services
 
     public class TasksServices : ITasksServices
     {
+        private NotionData _context;
 
+        public TasksServices(NotionData context)
+        {
+            _context = context;
+        }
+
+        public async Task<GenericRespones<TodoTasks>> AddTasks(int userId,TaskDto taks)
+        {
+            try
+            {
+                var user = await _context.users.FindAsync(userId);
+
+                if (user == null)
+                {
+                    return new GenericRespones<TodoTasks>("User not found", "NOT FOUND", 404, null, false);
+                }
+
+
+                TodoTasks userTask = new TodoTasks()
+                {
+                    Title = taks.Title,
+                    Description = taks.Description,
+                    IsCompleted = taks.IsCompleted,
+                    CreateAt = DateTime.UtcNow,
+                    UserId = userId
+                };
+
+                await _context.Tasks.AddAsync(userTask);
+                await _context.SaveChangesAsync();
+
+                return new GenericRespones<TodoTasks>("Task Added successfuly", "SUCCESS", 200, null, true);
+            }
+            catch (Exception ex)
+            {
+                return new GenericRespones<TodoTasks>("Internal server error", ex.Message, 500, null, false);
+            }
+            
+
+        }
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,21 +3,28 @@ using NotionAPI.Context;
 using NotionAPI.DTOs.User;
 using NotionAPI.Models;
 using NotionAPI.Utilites;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
 
 namespace NotionAPI.Services
 {
     public interface IuserService
     {
         public Task<GenericRespones<Users>> AddUser(UserRegister user);
-        public Task<GenericRespones<Users>> LoginUser(UserLogin user);
+        public Task<GenericRespones<string>> LoginUser(UserLogin user);
     }
     public class UserService : IuserService
     {
         public NotionData _context;
+        public IConfiguration _config;
 
-        public UserService(NotionData context)
+        public UserService(NotionData context,IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
         public async Task<GenericRespones<Users>> AddUser(UserRegister user)
         {
@@ -51,21 +58,45 @@ namespace NotionAPI.Services
             }
         }
 
-        public async Task<GenericRespones<Users>> LoginUser(UserLogin user)
+        public async Task<GenericRespones<string>> LoginUser(UserLogin user)
         {
             try
             {
-                Users userLogin = await _context.users.FirstOrDefaultAsync(u => u.Email == user.email);
+                Users userLogin = await _context.users.FirstOrDefaultAsync(u => u.Email == user.email && u.Password == user.password);
 
                 if(userLogin == null)
                 {
-                    return new GenericRespones<Users>("User not found","Not found",403,null,false);
+                    return new GenericRespones<string>("User not found","Not found",403,null,false);
                 }
-                return new GenericRespones<Users>("User login successful","",200,userLogin,true);
+
+                var claim = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userLogin.Id.ToString()),
+                    new Claim(ClaimTypes.Name,userLogin.Email),
+                    new Claim(ClaimTypes.Role,"user")
+                };
+
+                var key = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+                var creadentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claim,
+                expires: DateTime.UtcNow.AddMinutes(
+                Convert.ToDouble(_config["Jwt:ExpiryInMinutes"])
+                ),
+                    signingCredentials: creadentials);
+
+                var genToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return new GenericRespones<string>("User login successful","",200,genToken,true);
             }
             catch (Exception ex)
             {
-                return new GenericRespones<Users>("Internal server error",ex.Message,500,null,false);
+                return new GenericRespones<string>("Internal server error",ex.Message,500,null,false);
             }
         }
         
